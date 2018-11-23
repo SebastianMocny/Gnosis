f01c2604334860f651f5f5a2e93a6a16243108bc
Sebastian Mocny <biggi3lue@gmail.com>
2018-11-23 15:54:10 -0500

    just typing out my functions

--- c++/grid_challenge/grids.cpp
+++ c++/grid_challenge/grids.cpp
@@ -21,13 +21,29 @@ class Grid_2d {
 		// Simple and complex do the same thing in a different way, with a tradeoff in time/space complexity
 		// TODO: Overload these functions using a prediction method (Bayes prior?) to determine whether to use simple or complex and switch between the two
 		// This will require us to keep track of the calls made, and to compare #of calls to update vs. calls to query
-		void update_value_simple(int x, int y, int val); // O(1)
-		int query_span_simple(int x1, int y1, int x2, int y2); // O(n^2)
+		void update_value_simple(const int x, const int y, const int val); // O(1)
+		int query_span_simple(const int x1, const int y1, const int x2, const int y2); // O(n^2)
 
-		void update_value_complex(int x, int y, int val); // O(1) - See note below
-		int query_span_sum_complex(int x1, int y1, int x2, int y2); // O(1) if update has not been called. Otherwise, a worst case O(n^4) "update" operation is required
+		void update_value_complex(const int x, const int y, const int val); // O(1) - See note below
+		int query_span_sum_complex(const int x1, const int y1, const int x2, const int y2); // O(1) if update has not been called. Otherwise, a worst case O(n^4) "update" operation is required
 
 };
+
+void Grid_2d::update_sums_grid() {
+
+}
+
+void Grid_2d::update_value_simple(const int x, const int y, const int val) {
+}
+
+int Grid_2d::query_span_simple(const int x1, const int y1, const int x2, const int y2) {
+}
+
+void Grid_2d::update_value_complex(const int x, const int y, const int val) {
+}
+
+int Grid_2d::query_span_sum_complex(const int x1, const int y1, const int x2, const int y2) {
+}
 
 int main() {
 	return 0;
